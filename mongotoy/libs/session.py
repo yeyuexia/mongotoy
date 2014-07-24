6c4fefc09fec8a98c0e324051894b408437af83c
yeyuexia <yyxworld@gmail.com>
2014-07-24 21:47:48 +0800

    rename variable name in base.py and session.py, add loads_db_mapper

--- mongotoy/libs/session.py
+++ mongotoy/libs/session.py
@@ -4,28 +4,35 @@ import threading
 
 import pymongo
 
+from .base import get_collection_name
+
 _lock = threading.Lock()
 session = None
 
 
 class Session(object):
     def __init__(self, **kwargs):
-        self._lookup_table = kwargs.get("db_config", dict())
+        self._lookup_table = {}
+        self.mapper = kwargs.get("collection_mapper", dict())
 
-    def load_db_tables(self, configs):
-        self.configs = configs
+    def load_table_mapper(self, mapper):
+        for db, models in mapper:
+            self.mapper[db] = [get_collection_name(model) for model in models]
 
-    def _search_propable_db(self, table_name):
-        if table_name in self._lookup_table:
-            return self._lookup_table[table_name]
-        for db, tables in self.configs.iteritems():
-            if table_name in tables:
-                self._lookup_table[table_name] = db
+    def _search_propable_db(self, collection_name):
+        if collection_name in self._lookup_table:
+            return self._lookup_table[collection_name]
+        for db, collections in self.mapper.iteritems():
+            if collection_name in collections:
+                self._lookup_table[collection_name] = db
                 return db
+        raise ValueError(
+            "not get the collection named %s in collection mapper" % collection_name
+        )
 
-    def execute(self, table, func, fields):
-        db = self._search_propable_db(table)
-        return getattr(self.session[db][table], func)(**fields)
+    def execute(self, collection, func, fields):
+        db = self._search_propable_db(collection)
+        return getattr(self.session[db][collection], func)(**fields)
 
 
 class ReplicaSetSession(Session):
@@ -44,7 +51,7 @@ class ClientSession(Session):
         )
 
 
-def create_replica_set_session(db_uri, db_config, **kwargs):
+def create_replica_set_session(db_uri, collection_mapper=None, **kwargs):
     """
     Create a new connection to a MongoDB replica set.
     """
@@ -52,11 +59,11 @@ def create_replica_set_session(db_uri, db_config, **kwargs):
         global session
         if session:
             session.close()
-        session = ReplicaSetSession(db_uri, db_config, **kwargs)
+        session = ReplicaSetSession(db_uri, collection_mapper, **kwargs)
 
 
 def create_session(host, port=27017, max_pool_size=100,
-                   db_config=None, **kwargs):
+                   collection_mapper=None, **kwargs):
     """
     Create a new connection to a single MongoDB instance at host:port.
     """
@@ -65,5 +72,9 @@ def create_session(host, port=27017, max_pool_size=100,
         if session:
             session.close()
         session = ClientSession(
-            host, port, max_pool_size, db_config=None, **kwargs
+            host, port, max_pool_size, collection_mapper=None, **kwargs
         )
+
+
+def loads_db_mapper(mapper):
+    session.load_table_mapper(mapper)
